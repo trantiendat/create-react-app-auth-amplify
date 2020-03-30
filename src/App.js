93411d9a6e281a179a0ceb4354197176bfea73fe
Trần Tiến Đạt <trantiendat016@gmail.com>
2020-03-30 16:55:10 +0700

    Change text in home page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello World, this is 2nd commit</p>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
-  }
-}
+};
 
 export default withAuthenticator(App, true);
